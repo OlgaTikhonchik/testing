ca20b729d68abcf1b88a5489f14a6e787edcde2a
OlgaTikhonchik <olga882011@gmail.com>
2024-12-02 07:47:57 +0200

    Добавление секции

--- js/button-sidebar.js
+++ js/button-sidebar.js
@@ -9,3 +9,17 @@ document.addEventListener('DOMContentLoaded', function () {
       button.classList.toggle('active', !isActive);
     });
   });
+
+  // -----------sidebar-content
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.sidebar__links-content'); 
+    const list = document.querySelector('.sidebar__content-list'); 
+  
+   
+    button.addEventListener('click', function () {
+      const isActive = list.classList.contains('active');
+     list.classList.toggle('active', !isActive);
+      button.classList.toggle('active', !isActive);
+    });
+  });
